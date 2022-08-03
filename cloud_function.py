e7f1f63ad69bd218471354968414567104aa95a9
Jacek Hebda <jacek.hebda@devoteam.com>
2022-08-03 10:53:51 +0200

    Finishing touches

--- cloud_function.py
+++ cloud_function.py
@@ -3,7 +3,6 @@ from datetime import datetime as dt
 from google.cloud import aiplatform_v1, storage
 
 def main(event, context):
-
     path = event["id"].rsplit("/",1)[0].split("/",1)[1]
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(event['bucket'])
@@ -21,13 +20,10 @@ def main(event, context):
         location = entry['resource']['labels']['location']
         pipeline_id = entry['resource']['labels']['pipeline_job_id']
         name = f'projects/{project_id}/locations/{location}/pipelineJobs/{pipeline_id}'
+        delta = dt.now() - dt.strptime(timestamp[:-8].replace('T',' '), '%Y-%m-%d %H:%M:%S')
 
         request = aiplatform_v1.GetPipelineJobRequest(name=name)
         pipeline = client.get_pipeline_job(request=request)
-
-        print(pipeline.labels)
-        
-        delta = dt.now() - dt.strptime(timestamp[:-8].replace('T',' '), '%Y-%m-%d %H:%M:%S')
-        if delta.total_seconds() > pipeline.labels['wanna_sla']:
+        if delta.total_seconds() > 3600 * int(pipeline.labels['wanna_sla']): # 60 seconds * 60 minutes
             request = aiplatform_v1.CancelPipelineJobRequest(name=name)
             client.cancel_pipeline_job(request=request)
